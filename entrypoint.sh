bdb5b250bc977f73ff0660565544e47df59cc6dd
Chris Stefano <virtualstaticvoid@gmail.com>
2022-02-20 20:48:23 +0000

    refactored to build and release gem; instead of using `rake release`

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,24 +12,19 @@ EOF
 chmod 0600 ~/.gem/credentials
 set -x
 
+gem_name=$(ruby -r rubygems -e "puts Gem::Specification::load('$(ls *.gemspec)').name")
 gem_version=$(ruby -r rubygems -e "puts Gem::Specification::load('$(ls *.gemspec)').version")
+
+echo "::set-output name=gem_name::$gem_name"
 echo "::set-output name=gem_version::$gem_version"
 
-if git fetch origin "refs/tags/v$gem_version" >/dev/null 2>&1
-then
-  echo "Tag 'v$gem_version' already exists"
-  echo "::set-output name=new_version::false"
-else
-  echo "::set-output name=new_version::true"
+git config user.email "${GIT_EMAIL:-automated@example.com}"
+git config user.name "${GIT_NAME:-Automated Release}"
 
-  git config user.email "${GIT_EMAIL:-automated@example.com}"
-  git config user.name "${GIT_NAME:-Automated Release}"
+echo "Installing dependencies..."
+gem update bundler
+bundle install
 
-  echo "Installing dependencies..."
-  gem update bundler
-  bundle install
-
-  echo "Running gem release task..."
-  release_command="${RELEASE_COMMAND:-rake release}"
-  exec $release_command
-fi
+echo "Running gem release task..."
+rake build
+gem push pkg/#{gem_name}-#{gem_version}.gem
